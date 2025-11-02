8a9b2931160270292adb1fa1bfc581c924f01d7d
Rashmika119 <rashmikanethsarni119@gmail.com>
2025-11-02 10:25:50 +0530

    create the update method

--- src/vehicleRecord.resolver.ts
+++ src/vehicleRecord.resolver.ts
@@ -11,6 +11,7 @@ import { VehicleRecord } from './entity/vehicleRecord.entity';
 import { VehicleRecordService } from './vehicleRecord.service';
 import { VehicleRecordCreateDTO } from './dto/create-vehicleRecord.input';
 import { Vehicle } from './entity/vehicle.entity';
+import { VehicleRecordUpdateDTO } from './dto/update-vehicleRecord.input';
 @Resolver(() => VehicleRecord)
 export class VehicleRecordResolver {
   constructor(private vehicleRecordService: VehicleRecordService) { }
@@ -37,10 +38,14 @@ export class VehicleRecordResolver {
   remove(@Args('id') id: string) {
     return this.vehicleRecordService.remove(id);
   }
-
-  // @Query(()=>VehicleRecord,{name:'findByVin'})
-  // findOne(@Args('vin'))
-
+  @Mutation(() => VehicleRecord, { name: 'updateVehicleRecord' })
+  async updateVehicleRecord(
+    @Args('id') id: string,
+    @Args('updateData') updateData: VehicleRecordUpdateDTO,
+  ): Promise<VehicleRecord> {
+    console.log('Resolver: Updating vehicle record', id);
+    return this.vehicleRecordService.update(id, updateData);
+  }
   @ResolveField((of) => Vehicle)
   vehicle(@Parent() vehicleRecord: VehicleRecord) {
     return { __typename: 'Vehicle', vin: vehicleRecord.vin };
